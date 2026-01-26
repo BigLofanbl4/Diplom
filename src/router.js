e3da38bc80ce486ee9bf67dff31aeab1947a0d54
BigLofanbl4 <smallofanbl4@gmail.com>
2026-01-26 22:54:15 +0300

    feat(router): внедрил динамический роутинг на регулярных выражениях и асинхронный рендеринг

--- src/router.js
+++ src/router.js
@@ -14,43 +14,73 @@ export class Router {
   }
 
   handleDOMContentLoaded() {
-    document.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener("DOMContentLoaded", async () => {
       const url = window.location.pathname;
       console.log(url);
-      this.resolvePath(url);
+      await this.resolvePath(url);
     });
   }
 
   initNavigation() {
-    document.addEventListener("click", (event) => {
-      if (event.target.hasAttribute("data-spa-link")) {
+    document.addEventListener("click", async (event) => {
+      const link = event.target.closest("[data-spa-link]");
+      if (link) {
         event.preventDefault();
-        const url = event.target.getAttribute("href");
+        const url = link.getAttribute("href");
         history.pushState(null, null, url);
-        this.resolvePath(url);
+        await this.resolvePath(url);
       }
     });
   }
 
   handlePopstate() {
-    window.addEventListener("popstate", () => {
+    window.addEventListener("popstate", async () => {
       const url = window.location.pathname;
-      this.resolvePath(url);
+      await this.resolvePath(url);
     });
   }
 
-  resolvePath(url) {
-    const route = this.routes[url];
+  _matchRoute(routePath, url) {
+    const paramNames = [];
+    const regexPath = routePath.replace(/:([^\/]+)/g, (match, paramName) => {
+      paramNames.push(paramName);
+      return '([^\/]+)';
+    });
+
+    const regex = new RegExp(`^${regexPath}$`);
+    const match = url.match(regex);
+
+    if (!match) return null;
+
+    const params = {};
+    match.slice(1).forEach((value, index) => params[paramNames[index]] = value);
+    return params;
+  }
+
+  async resolvePath(url) {
+    let route = null;
+    let params = {};
+
+    for (const path in this.routes) {
+      const matchedParams = this._matchRoute(path, url);
+      if (matchedParams) {
+        route = this.routes[path];
+        params = matchedParams;
+        break;
+      }
+    }
+
     if (!route) {
       console.log("404");
       // отрисовка страницы 404
     }
 
     if (!this.currentLayout || route.Layout !== this.currentLayout.constructor) {
-      if (this.currentLayout) this.currentLayout.destroy();
-
+      if (this.currentLayout) {
+        this.currentLayout.destroy();
+      }
       this.currentLayout = new route.Layout();
-      this.currentLayout.draw();
+      await this.currentLayout.draw();
 
       if (this.currentComponent) this.currentComponent.destroy();
     }
@@ -58,8 +88,11 @@ export class Router {
     if (route.Component && (!this.currentComponent || this.currentComponent.constructor !== route.Component)) {
       if (this.currentComponent) this.currentComponent.destroy();
 
-      this.currentComponent = new route.Component();
-      this.currentComponent.draw();
+      this.currentComponent = new route.Component(params);
+      await this.currentComponent.draw();
+    } else if (!route.Component) {
+      if (this.currentComponent) this.currentComponent.destroy();
+      this.currentComponent = null;
     }
   }
 }
