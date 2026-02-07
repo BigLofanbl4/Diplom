3f348245ccba0d8d89aca531be0e8ffd9b026cef
BigLofanbl4 <smallofanbl4@gmail.com>
2026-02-07 23:56:52 +0300

    Черновик страницы курса
    Перевел страницу курса на шаблонную функцию, добавил рендер модулей/уроков и обработчики создания/редактирования/удаления через модалки.

--- src/components/admin_courses/CoursePage.js
+++ src/components/admin_courses/CoursePage.js
@@ -1,21 +1,122 @@
-import template from "./CoursePage.html?raw";
+import { CoursePageTemplate } from "./CoursePage.template.js";
+import ModuleForm from "./ModuleForm";
+import LessonForm from "./LessonForm";
+import ModalWithComponent from "../common/ModalWithComponent/ModalWithComponent";
+import CourseService from "../../services/CourseService";
+import ModuleService from "../../services/ModuleService";
+import LessonService from "../../services/LessonService";
 
+
+function lessonRenderer(lesson) {
+  return `
+    <li class="course-lesson" data-lesson-id="${lesson.id}">
+      <h5 class="course-lesson__title">
+        <span class="course-lesson__number">Урок ${lesson.lesson_number}:</span>
+        <span class="course-lesson__name">${lesson.title}</span>
+      </h5>
+      <p class="course-lesson__desc">${lesson.description}</p>
+      <div class="course-lesson__badges">
+        <span class="course-lesson__badge course-lesson__badge--danger">
+          <i class="fa-regular fa-rectangle-list"></i>
+          Отсутствуют тесты
+        </span>
+        <span class="course-lesson__badge course-lesson__badge--danger">
+          <i class="fa-regular fa-file-lines"></i>
+          Отсутствуют материалы
+        </span>
+      </div>
+      <div class="course-lesson__actions">
+        <button class="btn btn-secondary course-lesson__edit-btn course-lesson__action-btn" data-action="update">
+          <i class="fa-solid fa-pen"></i>
+        </button>
+        <button class="btn btn-danger course-lesson__delete-btn course-lesson__action-btn" data-action="delete">
+          <i class="fa-solid fa-trash"></i>
+        </button>
+      </div>
+    </li>
+  `
+}
+
+function moduleRenderer(module, lessons, isModuleHidden = true) {
+  const moduleLessons = lessons.filter(lesson => module.id === lesson.module_id);
+  const lessonsHTML = moduleLessons.map(lesson => lessonRenderer(lesson)).join("");
+  console.log(module);
+  return `
+    <li class="course-module" data-module-id="${module.id}" data-lessons-hidden="true">
+          <div class="course-module__header">
+            <h4 class="course-module__title">
+              <span class="course-module__number">Модуль ${module.module_number}:</span>
+              <span class="course-module__name">${module.title}</span>
+            </h4>
+            <div class="course-module__actions">
+              <button class="btn btn-secondary course-module__edit-btn course-module__action-btn" data-action="update">
+                <i class="fa-solid fa-pen"></i>
+              </button>
+              <button class="btn btn-danger course-module__delete-btn course-module__action-btn" data-action="delete">
+                <i class="fa-solid fa-trash"></i>
+              </button>
+              <button class="course-module__toggle">
+                <i class="fa-solid fa-caret-down"></i>
+              </button>
+            </div>
+          </div>
+          <div class="course-lessons">
+            <ul class="course-lessons__list">
+              ${lessonsHTML}
+            </ul>
+            <div class="course-lessons__controls">
+              <button class="btn btn-primary course-lessons__add-btn" data-action="create" data-entity="lesson">
+                Создать урок
+              </button>
+            </div>
+          </div>
+        </li>
+  `;
+}
 
 export default class CoursePage {
-  constructor() {
-    this.template = template;
-    this.data = {
-      "title": "Курс Frontend-разработка",
-      "description": "Курс по HTML, CSS, JS, Vue.js",
-    };
+  constructor({ id }) {
+    this.template = null;
+    this.id = Number(id);
+    this.data = {};
+    this.generalForm = null;
+    this.modulesContainer = null;
+    this.moduleList = null;
+    this.modulesHTML = null;
   }
 
+  async fetchData() {
+    try {
+      this.data = await CourseService.getById(this.id);
+      console.log(this.data);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    this.template = CoursePageTemplate(this.data);
+    this.renderModules();
+  }
+
+  renderModules() {
+    this.modulesHTML = this.data.modules.map(module => moduleRenderer(module, this.data.lessons)).join("");
+  }
 
   mount() {
     const componentContainer = document.getElementById("component");
     componentContainer.innerHTML = this.template;
     this.generalForm = componentContainer.querySelector("#course-general-form");
     this.modulesContainer = componentContainer.querySelector(".course-modules__body");
+    this.moduleList = componentContainer.querySelector(".course-modules__list");
+    this.mountModules();
+  }
+
+  mountModules() {
+    if (this.modulesHTML && this.moduleList) {
+      this.moduleList.innerHTML = this.modulesHTML;
+    }
   }
 
   handleEvents() {
@@ -31,17 +132,135 @@ export default class CoursePage {
       }
     });
 
-    this.modulesContainer.addEventListener("click", (e) => {
+    this.generalForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const formData = new FormData(this.generalForm);
+      const payload = Object.fromEntries(formData);
+      try {
+        await CourseService.update(this.id, payload);
+        this.data.title = payload.title;
+        this.data.description = payload.description;
+      } catch (error) {
+        console.error(error);
+      }
+      const formControls = this.generalForm.querySelector(".course__general-form-controls");
+      formControls.style.display = "none";
+    })
+
+    document.getElementById("module-create-btn").addEventListener("click", async () => {
+      const modalInstance = new ModalWithComponent({
+        Component: ModuleForm,
+        componentProps: {
+          containerElementId: "modal-component",
+          successHandler: async () => {
+            await this.fetchData();
+            this.renderModules();
+            this.mountModules();
+            modalInstance.destroy();
+          },
+          cancelHandler: () => modalInstance.destroy(),
+          courseId: this.id
+        }
+      });
+
+      await modalInstance.draw();
+    });
+
+    this.modulesContainer.addEventListener("click", async (e) => {
       const target = e.target;
       if (!target.closest("[data-action]") && target.closest(".course-module__header")) {
         const moduleContainer = target.closest("[data-module-id]");
         if (!moduleContainer) return;
         moduleContainer.dataset.lessonsHidden = moduleContainer.dataset.lessonsHidden === "true" ? "false" : "true";
       }
+
+      if (target.closest("[data-module-id]") && target.closest('[data-action="update"]')) {
+        const moduleId = Number(target.closest("[data-module-id]").dataset.moduleId);
+        const modalInstance = new ModalWithComponent({
+          Component: ModuleForm,
+          componentProps: {
+            id: moduleId,
+            containerElementId: "modal-component",
+            successHandler: async () => {
+              await this.fetchData();
+              this.renderModules();
+              this.mountModules();
+              modalInstance.destroy();
+            },
+            cancelHandler: () => modalInstance.destroy(),
+          }
+        });
+        await modalInstance.draw();
+      }
+
+      if (target.closest("[data-module-id]") && target.closest('[data-action="delete"]')) {
+        const moduleContainer = target.closest('[data-module-id]');
+        const moduleId = Number(target.closest("[data-module-id]").dataset.moduleId);
+
+        const accept = confirm(`Удалить модуль ${moduleId}?`);
+        if (!accept) return;
+
+        const success = await ModuleService.delete(moduleId);
+        if (success) {
+          moduleContainer.remove();
+        }
+      }
+
+      if (target.closest("[data-lesson-id]") && target.closest('[data-action="update"]')) {
+        const lessonId = Number(target.closest("[data-lesson-id]").dataset.lessonId);
+        const modalInstance = new ModalWithComponent({
+          Component: LessonForm,
+          componentProps: {
+            id: lessonId,
+            containerElementId: "modal-component",
+            successHandler: async () => {
+              await this.fetchData();
+              this.renderModules();
+              this.mountModules();
+              modalInstance.destroy();
+            },
+            cancelHandler: () => modalInstance.destroy(),
+          }
+        });
+        await modalInstance.draw();
+      }
+
+      if (target.closest("[data-lesson-id]") && target.closest('[data-action="delete"]')) {
+        const lessonContainer = target.closest('[data-lesson-id]');
+        const lessonId = Number(lessonContainer.dataset.lessonId);
+        const accept = confirm(`Удалить урок ${lessonId}?`)
+        if (!accept) return;
+        const success = await LessonService.delete(lessonId);
+        if (success) {
+          lessonContainer.remove();
+        }
+      }
+
+      if (target.closest('[data-action="create"]') && target.closest('[data-entity="lesson"]')) {
+        const moduleId = Number(target.closest('[data-module-id]').dataset.moduleId);
+        const modalInstance = new ModalWithComponent({
+          Component: LessonForm,
+          componentProps: {
+            containerElementId: "modal-component",
+            successHandler: async () => {
+              await this.fetchData();
+              this.renderModules();
+              this.mountModules();
+              modalInstance.destroy();
+            },
+            cancelHandler: () => modalInstance.destroy(),
+            moduleId: moduleId,
+            courseId: this.id,
+          }
+        });
+        await modalInstance.draw();
+      }
     });
   }
 
-  draw() {
+  async draw() {
+    await this.fetchData();
+    this.render();
     this.mount();
     this.handleEvents();
   }
--- src/components/admin_courses/CoursePage.template.js
+++ src/components/admin_courses/CoursePage.template.js
@@ -1,38 +1,42 @@
-<article class="course">
-  <header class="course__header">
-    <h1 class="course__header-title">Курс ID: 1</h1>
-    <form class="course__general-form" id="course-general-form">
-      <div class="form-group course__general-form-group">
-        <label for="course-title">Изменить название:</label>
-        <input
-          type="text"
-          value="Курс Frontend-разработка"
-          id="course-title"
-          name="title"
-          class="course__title"
-        >
+export const CoursePageTemplate = (course) => {
+  return `
+      <article class="course" data-course-id="${course.id}">
+      <header class="course__header">
+        <h1 class="course__header-title">Курс ID: ${course.id}</h1>
+        <form class="course__general-form" id="course-general-form">
+          <div class="form-group course__general-form-group">
+            <label for="course-title">Изменить название:</label>
+            <input
+              type="text"
+              value="${course.title}"
+              id="course-title"
+              name="title"
+              class="course__title"
+            >
+          </div>
+          <div class="form-group course__general-form-group">
+            <label for="course-description">Изменить описание:</label>
+            <textarea class="course__description" id="course-description" name="description">${course.description}</textarea>
+          </div>
+          <div class="course__general-form-controls">
+            <button class="btn btn-primary" data-action="save">Сохранить</button>
+            <button class="btn btn-secondary" data-action="cancel">Отменить</button>
+          </div>
+        </form>
+      </header>
+      <div class="course-modules">
+        <div class="course-modules__header">
+          <h2>Модули</h2>
+          <button class="btn btn-primary course-modules__add-btn" id="module-create-btn" data-action="create" data-entity="module">
+            <i class="fa-solid fa-plus"></i>
+          </button>
+        </div>
+        <div class="course-modules__body">
+          <ul class="course-modules__list">
+            
+          </ul>
+        </div>
       </div>
-      <div class="form-group course__general-form-group">
-        <label for="course-description">Изменить описание:</label>
-        <textarea class="course__description" id="course-description" name="description">Курс по HTML, CSS, JS, Vue.js</textarea>
-      </div>
-      <div class="course__general-form-controls">
-        <button class="btn btn-primary" data-action="save">Сохранить</button>
-        <button class="btn btn-secondary" data-action="cancel">Отменить</button>
-      </div>
-    </form>
-  </header>
-  <div class="course-modules">
-    <div class="course-modules__header">
-      <h2>Модули</h2>
-      <button class="btn btn-primary course-modules__add-btn" id="module-create-btn" data-action="create" data-entity="module">
-        <i class="fa-solid fa-plus"></i>
-      </button>
-    </div>
-    <div class="course-modules__body">
-      <ul class="course-modules__list">
-        
-      </ul>
-    </div>
-  </div>
-</article>
+    </article>
+  `
+};
